439edec7c88218307c310dff9f9e70900f21c60f
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-04-28 12:12:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pandas as pd
 # Chargement en local en pikle
 liste_films = pd.read_pickle("liste_films.pkl.gz")
 df_genres2 = pd.read_pickle("df_genres2.pkl.gz")
+df_films_note2 = pd.read_pickle("df_films_note2.pk.gz")
 
-
+# Configuration de la page
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
     layout="wide",
@@ -17,13 +18,12 @@ st.title("Bienvenue dans notre humble application de remmandation de film")
 # Header
 st.header("Dis moi quel film tu aimes et je t'en ferai aimer d'autres")
  
-# multi select box
-# first argument takes the box title
-# second argument takes the options to show
 
 liste_films_déroulante_films = ["Tape le film que tu aimes"] + list(liste_films["primaryTitle"])
 
 liste_films_déroulante_genres = df_genres2["genres"]
+
+liste_deroulante_acteur = df_films_note2["primaryName"]
 
 
    
@@ -36,7 +36,7 @@ with st.form("form 4"):
             genres = st.multiselect("Genres : ", liste_films_déroulante_genres)
             st.write("Tu as choisis", len(genres), 'genre(s)')
         with col3 : 
-            acteurs = st.multiselect("Acteurs : ", ["Leo", "Alain", "Clint", "Marylin"])
+            acteurs = st.multiselect("Acteurs : ", liste_deroulante_acteur)
             st.write("Tu as choisis", len(acteurs), 'acteur(trice)')
         with col4 : 
             année = st.slider("Année", 1913, 2023)
