4e66d442c25815c6405d9ac0ffcafcab6a9d8c91
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-16 17:09:51 +0200

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+import requests
 
 
 # Chargement en local en pikle
@@ -18,69 +20,92 @@ st.set_page_config(
     layout="wide",
     page_icon="🎞️")
 
-#title
-st.title("Bienvenue dans notre humble application de remmandation de film")
+# Titre
+st.title("🎥Bienvenue dans notre humble application de recommandation de film")
 
 # Header
 st.header("Dis moi quel film tu aimes et je t'en ferai aimer d'autres")
  
-#liste
+# listes
 liste_films_deroulante_films = ["Tape le film que tu aimes"] + list(liste_films["primaryTitle"])
 liste_films_deroulante_genres = list(df_genres2["genres"])
 liste_deroulante_acteur = list(df_films_note2["primaryName"])
 liste_deroulante_annee = list(df_annee["startYear"])
 
 # Subheader
-st.subheader("Choisi obligatoirement ton film préféré")
+st.subheader("Choisi ou tape ton film préféré. \nTu peux compléter ta recherche avec tes genres et acteurs/trices préférées mais ce n'est pas obligatoire 😎")
 
-#Machine Learning Partie 1
-#récupération des noms des colonnes sans prendre tconst+primaryTitle+originalTitle+averageRating+numVotes+nconst+primaryProfession+knownForTitles
-colonnes_ml = df_merge_finalML.columns[5:]
-#création de la variable X qui prends en variables explicatives toutes les colonnes numériques sauf (voir celles ci-dessus)
+# Machine Learning Partie 1
+# Récupération des noms des colonnes sans prendre const+primaryTitle+originalTitle+averageRating+numVotes+nconst+primaryProfession+knownForTitles
+colonnes_ml = df_merge_finalML.columns[7:]
+
+# Création de la variable X qui prends en variables explicatives toutes les colonnes numériques sauf (voir celles ci-dessus)
 X = df_merge_finalML.loc[:, colonnes_ml]
-#initialisation du model avec 4 voisins
-distanceKNN = NearestNeighbors(n_neighbors = 4).fit(X)
-    
-films = st.selectbox("Films : ",liste_films_deroulante_films)
-st.write(films, ". Bon choix ;)")
 
-# Subheader
-st.subheader("Tu peux également affiner parmi les listes de choix suivantes")
+# Initialisation du model avec 4 voisins
+distanceKNN = NearestNeighbors(n_neighbors = 4, metric = "cosine", algorithm = "brute").fit(X)
 
-
-with st.form("form 4"):
+# Bloc de mise en forme pour utilisateur
+with st.form("form_4"):
         col1, col2, col3 = st.columns(3)
         with col1 : 
-            genres = st.multiselect(label = "Genres : ", options = liste_films_deroulante_genres)
-            st.write("Tu as choisis", len(genres), 'genre(s)')
+            films = st.selectbox("Films : ", liste_films_deroulante_films)
         with col2 : 
-            acteurs = st.multiselect(label = "Acteurs : ", options = liste_deroulante_acteur)
-            st.write("Tu as choisis", len(acteurs), 'acteur(trice)')
+            genres = st.multiselect(label = "Genres : ", options = liste_films_deroulante_genres)
+            #st.write("Tu as choisis", genres, 'genre(s)')
         with col3 : 
-            start_year, end_year = st.select_slider(label = "Sélectionne une plage d'année", options = df_annee["startYear"], value = (1913, 2023))
-            st.write("Tu as choisis une plage d'année entre", start_year, 'et', end_year)
+            acteurs = st.multiselect(label = "Acteurs : ", options = liste_deroulante_acteur)
+            #st.write("Tu as choisis", acteurs, 'acteur(trice)')
         submit = st.form_submit_button("Soumettre")
-        
-st.write("Tu as choisi : {}, qui a pour genre(s) {}, avec les acteurs(trices) {}, et dont les années sont comprises entre {} et {}.".format
-                 (films, "/".join(genres), "/".join(acteurs), start_year, end_year))           
-if submit:
-    
+     
+if submit:      
 # Machine Learning Partie 2
-#création de la liste avec le film choisi par l'utilisateur
-        liste_du_film = [films]
+# Création liste de film
+        liste_du_film = films
 
-#obtenir tous les renseignements du film
+# Obtenir tous les renseignements du film
         df_film_choisi = df_merge_finalML[(df_merge_finalML["primaryTitle"] == films) | (df_merge_finalML["originalTitle"] == films)]
 
-# on ne selectionne que les colonnes contenant des booleens sur la ligne du film choisi
-        film_choisi = df_film_choisi.iloc[:, 5:]
+# On ne selectionne que les colonnes contenant des booleens sur la ligne du film choisi
+        film_choisi = df_film_choisi.iloc[:, 7:]
 
-#création de la matrice pour rechercher les index des plus proches voisins
+# Création de la matrice pour rechercher les index des plus proches voisins
         matrice_des_plus_proches_voisins = distanceKNN.kneighbors(film_choisi)
 
-#création de la liste des suggestions à partir de la matrice
-        suggestion = df_merge_finalML.iloc[matrice_des_plus_proches_voisins[1][0][1:], 1].values
-        st.write("On peut remplacer", films, "par :", [suggestion])
+# Création de la liste des suggestions à partir de la matrice
+        suggestion = df_merge_finalML.iloc[matrice_des_plus_proches_voisins[1][0][1:], 0].values
+        st.write("Je te propose :")
+
+# Création d'une variable pour récupérer le tconst
+        #tconst = df_merge_finalML.iloc[matrice_des_plus_proches_voisins[1][0][1:]]["tconst"].values
+# Création d'une variable pour récupérer le nom du film
+        nom_du_film = df_merge_finalML.loc[matrice_des_plus_proches_voisins[1][0][1:]]["primaryTitle"].values
+
+# Api pour affiche de films
+        url_api = "http://www.omdbapi.com/?i="
+        key_api = "&apikey=ec8d0879"
+
+# Création de colones
+        col1 = st.columns(3)
+     
+# Boucle sur tconst et suggestion
+        for film, nom, colonnes in zip(suggestion, nom_du_film, col1):
+            with colonnes :
+                url = url_api + str(film) + key_api
+                
+                try:
+                    response = requests.get(url)
+                    response.raise_for_status()
+                    data = response.json()
+                    url_image = data['Poster']
+                    st.image(url_image, width=200)
+                except requests.exceptions.RequestException as e:
+                    print("Une erreur est survenue lors de l'appel à l'API :", e)
+                st.write(' - {}'.format(nom))
+
+
+# Subheader
+        st.subheader("Bon visionnage ! ❤")
         
 
 # Subheader
